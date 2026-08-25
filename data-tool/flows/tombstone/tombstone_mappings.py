ff34079ff9da6e668fca505a35eb38ac8cd3a686
Yogesh Manni <yogeshmanni786@gmail.com>
2026-08-25 14:48:42 -0700

    feat: add railway changes

--- data-tool/flows/tombstone/tombstone_mappings.py
+++ data-tool/flows/tombstone/tombstone_mappings.py
@@ -68,6 +68,9 @@ class EventFilings(str, Enum):
     # CONVOTHER Change of Directors
     CONVOTHER_OTCDR = 'CONVOTHER_OTCDR'
 
+    # Change of Name
+    FILE_OTNCN = 'FILE_OTNCN'
+
     # Consent Continuation Out
     FILE_CONTO = 'FILE_CONTO'
 
@@ -130,9 +133,13 @@ class EventFilings(str, Enum):
     FILE_ADVD2 = 'FILE_ADVD2'
     FILE_ADVDS = 'FILE_ADVDS'
     FILE_OTVDS = 'FILE_OTVDS'
+    FILE_OTDIS = 'FILE_OTDIS'
 
     # CONVOTHER Dissolution
     CONVOTHER_OTVDS = 'CONVOTHER_OTVDS'
+    # CONVOTHER Dissolution (OTDIS - historical dissolution type)
+    CONVOTHER_OTDIS = 'CONVOTHER_OTDIS'
+
     ## admin
     SYSDA_NULL = 'SYSDA_NULL'
     SYSDS_NULL = 'SYSDS_NULL'
@@ -279,6 +286,9 @@ EVENT_FILING_LEAR_TARGET_MAPPING = {
     # CONVOTHER Change of Directors
     EventFilings.CONVOTHER_OTCDR: 'changeOfDirectors',
 
+    # Change of Name
+    EventFilings.FILE_OTNCN: ['conversion', 'changeOfName'],
+
     EventFilings.FILE_CONTO: 'consentContinuationOut',
     EventFilings.FILE_COUTI: 'continuationOut',
 
@@ -329,9 +339,12 @@ EVENT_FILING_LEAR_TARGET_MAPPING = {
     EventFilings.FILE_ADVD2: ['dissolution', 'voluntary'],
     EventFilings.FILE_ADVDS: ['dissolution', 'voluntary'],
     EventFilings.FILE_OTVDS: ['dissolution', 'voluntary'],
+    EventFilings.FILE_OTDIS: ['dissolution', 'voluntary'],
+    
 
     # CONVOTHER Dissolution
     EventFilings.CONVOTHER_OTVDS: ['dissolution', 'voluntary'],
+    EventFilings.CONVOTHER_OTDIS: ['dissolution', 'voluntary'],
 
     EventFilings.SYSDA_NULL: ['dissolution', 'administrative'],
     EventFilings.SYSDS_NULL: ['dissolution', 'administrative'],
@@ -453,6 +466,8 @@ EVENT_FILING_DISPLAY_NAME_MAPPING = {
 
     # CONVOTHER Change of Directors
     EventFilings.CONVOTHER_OTCDR: 'Notice of Change of Directors',
+    # Change of Name
+    EventFilings.FILE_OTNCN: 'Notice of Change of Name',
 
     EventFilings.FILE_CONTO: '6 Months Consent to Continue Out',
     EventFilings.FILE_COUTI: 'Instrument of Continuation Out',
@@ -488,9 +503,12 @@ EVENT_FILING_DISPLAY_NAME_MAPPING = {
     EventFilings.FILE_ADVD2: 'Application for Dissolution (Voluntary Dissolution)',
     EventFilings.FILE_ADVDS: 'Application for Dissolution (Voluntary Dissolution)',
     EventFilings.FILE_OTVDS: 'Voluntary Dissolution',
+    EventFilings.FILE_OTDIS: 'Dissolution',
+    
 
     # CONVOTHER Dissolution
     EventFilings.CONVOTHER_OTVDS: 'Voluntary Dissolution',
+    EventFilings.CONVOTHER_OTDIS: 'Dissolution',
 
     EventFilings.SYSDA_NULL: None,  # admin - status Administrative Dissolution
     EventFilings.SYSDS_NULL: None,  # admin - status Administrative Dissolution
